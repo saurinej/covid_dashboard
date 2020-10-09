86596231b654bfaf245758e6951fa33558ec6941
saurinej <saurinej1@gmail.com>
2020-10-09 14:58:55 -0400

    Add comments

--- covid_dashboard/DateTimeComparer.cs
+++ covid_dashboard/DateTimeComparer.cs
@@ -5,8 +5,35 @@ using System.Text;
 
 namespace covid_dashboard
 {
+    /// <summary>
+    /// Used to compare two date objects. Two objects are equal if they are on the same day after a specific hour or the next day before the specific hour. Default hour is 1400.
+    /// </summary>
     class DateTimeComparer : IEqualityComparer<DateTime>
     {
+
+        private int hour;
+
+        public DateTimeComparer()
+        {
+            hour = 14;
+        }
+        /// <summary>
+        /// Constructor which allows a custom hour to compare dates with.
+        /// </summary>
+        /// <param name="hour">An integer between 0 and 23.</param>
+        public DateTimeComparer(int hour)
+        {
+            if (hour < 0 || hour > 23)
+            {
+                hour = 14;
+            }
+            else
+            {
+                this.hour = hour;
+            }
+                
+        }
+
         public bool Equals(DateTime date1, DateTime date2)
         {
             //if both objects are null, they are equal
@@ -14,8 +41,8 @@ namespace covid_dashboard
             //if only one object is null, they are not equal
             else if (date1 == null || date2 == null) return false;
             //if hour of either is before 1400, subtract one day
-            if (date1.Hour < 14) date1 = date1.AddDays(-1);
-            if (date2.Hour < 14) date2 = date2.AddDays(-1);
+            if (date1.Hour < hour) date1 = date1.AddDays(-1);
+            if (date2.Hour < hour) date2 = date2.AddDays(-1);
             //return true if day, month and year are equal after adjusting day for time collected. Return false if not
             if (date1.Day == date2.Day && date1.Month == date2.Month && date1.Year == date2.Year)
             {
@@ -27,10 +54,14 @@ namespace covid_dashboard
             }
 
         }
-
+        /// <summary>
+        /// Returns the hashcode of a date object. Hashcode calculated using year, month, and day.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public int GetHashCode(DateTime obj)
         {
-            if (obj.Hour < 14) obj = obj.AddDays(-1);
+            if (obj.Hour < hour) obj = obj.AddDays(-1);
             long hCode = obj.Year ^ obj.Day * obj.Month;
             return hCode.GetHashCode();
         }
